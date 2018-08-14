e7767b53eb355054ca36a59752171a31499a77dd
tannamatos <39965004+tannamatos@users.noreply.github.com>
2018-08-14 11:22:40 -0300

    Bean

---
+++ BluVitality/src/java/br/com/Sistema/Bean/CargosBean.java
@@ -0,0 +1,45 @@
+package br.com.Sistema.Bean;
+
+/**
+ * @author Nattana Matos
+ */
+public class CargosBean {
+
+    private int id;
+    private String nome;
+    private double salario;
+    private int carga_horaria;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getCarga_horaria() {
+        return carga_horaria;
+    }
+
+    public void setCarga_horaria(int carga_horaria) {
+        this.carga_horaria = carga_horaria;
+    }
+   
+}
---
+++ BluVitality/src/java/br/com/Sistema/Bean/FuncionariosBean.java
@@ -0,0 +1,46 @@
+package br.com.Sistema.Bean;
+
+/**
+ * @author Nattana Matos
+ */
+public class FuncionariosBean {
+
+    private int id;
+    private int id_usuario;
+    private int id_cargo;
+    private boolean ativo;
+    private UsuariosBean usuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(int id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+}
