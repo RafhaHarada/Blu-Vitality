80e347b734b0abf96c44815a0eeefb236c57afbd
Rafael Harada <RafhaHarada@gmail.com>
2018-08-14 10:24:33 -0300

    Indentação

--- BluVitality/src/java/br/com/Sistema/Database/EstruturaSQL.sql
+++ BluVitality/src/java/br/com/Sistema/Database/EstruturaSQL.sql
@@ -8,16 +8,16 @@ CREATE TABLE usuarios(
     estado_civil VARCHAR(100) NOT NULL,
     idade TINYINT NOT NULL,
     sexo CHAR NOT NULL,
-	login VARCHAR(100) NOT NULL,
-	senha VARCHAR(100) NOT NULL,
-	cpf VARCHAR(100) NOT NULL,
-	rg VARCHAR(100) NOT NULL,
-	telefone VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	endereco VARCHAR(100) NOT NULL,
-	naturalidade VARCHAR(100) NOT NULL,
-	data_nascimento DATE NOT NULL,
-	nome_fic VARCHAR(100) NOT NULL
+    login VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    cpf VARCHAR(100) NOT NULL,
+    rg VARCHAR(100) NOT NULL,
+    telefone VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    naturalidade VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    nome_fic VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE cargos(
