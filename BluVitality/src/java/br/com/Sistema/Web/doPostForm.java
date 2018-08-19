4343cd6b64aeeb3e06246f61cf574863483010c1
RafhaHarada <RafhaHarada@gmail.com>
2018-08-19 18:01:49 -0300

    Moldes doGet e doPost

---
+++ BluVitality/src/java/br/com/Sistema/Web/doPostForm.java
@@ -0,0 +1,34 @@
+//package br.com.Sistema.Web;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * @author YOU
+// */
+//
+//@WebServlet("url")
+//public class doPostForm extends HttpServlet{
+//
+//    protected doPostForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//        //NomeBean nome = new NomeBean;
+//        //nome.setPropriedade(req.getParameter(""));
+//        
+//        //                new NomeDAO.atividade(nome);
+//        boolean mudanca = true;
+//        if(mudanca){
+//            resp.sendRedirect("/BluVitality/'Anterior a esta pagina'");
+//        }
+//        else{
+//            System.out.println("Erro");
+//        }
+//    }
+//    
+//    
+//}
