8834a762be2cc3c689d0038c0dea0e8d4d2dbaa9
Rafael Harada <RafhaHarada@gmail.com>
2018-08-20 11:31:48 -0300

    Atualização de JavaScript
    Guardando JS no custom

--- BluVitality/web/resources/js/custom.js
+++ BluVitality/web/resources/js/custom.js
@@ -3,13 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
-  });
+});
 
-  // Or with jQuery
+// Or with jQuery
 
-  $(document).ready(function(){
+$(document).ready(function () {
+    //Menu retratil
+    $('.collapsible').collapsible();
+
+    //Efeito parallax
     $('.parallax').parallax();
+
+    //Mobile Nav Menu
+    $('.sidenav').sidenav();
+
+    $('.fixed-action-btn').floatingActionButton({
+        direction: 'left',
+        toolbarEnabled: true
+    });
+
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        i18n: {
+            selectMonths: true, //Creates a dropdown to control month
+            selectYears: 15, //Creates a dropdown of 15 years to control year
+            //The title label to use for the month nav buttons
+            labelMonthNext: 'Proximo Mês',
+            labelMonthPrev: 'Mês Anterior',
+            //The title label to use for the dropdown selectors
+            labelMonthSelect: 'Selecionar Mês',
+            labelYearSelect: 'Selecionar Ano',
+            cancel: 'Cancelar',
+            clear: 'Limpar',
+            done: 'Ok',
+            previousMonth: '‹',
+            nextMonth: '›',
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+            //Materialize modified
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            today: 'Hoje',
+            clear: 'Limpar',
+                    close: 'Fechar',
+            //The format to show on the `input` element
+        }
+
+    });
+    $(document).ready(function () {
+        $('.pushpin').pushpin();
+    });
+});
+
+$('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
   });
