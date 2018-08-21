81eb7ccbcd424731e4a8cd9ea04fc8e7c43b1361
Rafael Harada <RafhaHarada@gmail.com>
2018-08-21 11:30:51 -0300

    Custom

--- BluVitality/web/resources/js/custom.js
+++ BluVitality/web/resources/js/custom.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
             weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
             today: 'Hoje',
             clear: 'Limpar',
-            close: 'Fechar',
+                    close: 'Fechar',
             //The format to show on the `input` element
         }
 
@@ -65,7 +65,10 @@ $(document).ready(function () {
         coverTrigger: false
     });
     
-    $('.tabs').tabs();
+    $('.tabs').tabs({
+        swipeable: true,
+        onShow: true
+    });
 });
 
 $('.pushpin-demo-nav').each(function () {
