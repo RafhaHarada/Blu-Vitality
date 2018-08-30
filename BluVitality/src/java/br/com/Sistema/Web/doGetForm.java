acf8fbbea011863aa33e19a6c32f53ef49d5e11d
RafhaHarada <RafhaHarada@gmail.com>
2018-08-30 11:33:30 -0300

    Form do Get atualizado

--- BluVitality/src/java/br/com/Sistema/Web/doGetForm.java
+++ BluVitality/src/java/br/com/Sistema/Web/doGetForm.java
@@ -1,7 +1,6 @@
 //package br.com.Sistema.Web;
 //
 //import java.io.IOException;
-//import java.io.PrintWriter;
 //import javax.servlet.ServletException;
 //import javax.servlet.annotation.WebServlet;
 //import javax.servlet.http.HttpServlet;
@@ -14,14 +13,10 @@
 //
 //@WebServlet("url")
 //public class doGetForm extends HttpServlet{
-//
-//    protected doGetForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        PrintWriter out = resp.getWriter();
-//        PageRuler.PageTopo(out);
-//        
-//        out.print("//Body Content");
-//        
-//        PageRuler.PageRodape(out);
+//    
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
 //    }
 //    
 //    
