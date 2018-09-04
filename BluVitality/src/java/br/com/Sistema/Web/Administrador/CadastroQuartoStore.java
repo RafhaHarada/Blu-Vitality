fc47d232912ed07ce7dc504372893830a70a04e7
Gustavo <gugaarodrigues@gmail.com>
2018-09-04 11:33:35 -0300

    ...

--- BluVitality/src/java/br/com/Sistema/Web/Administrador/CadastroQuartoStore.java
+++ BluVitality/src/java/br/com/Sistema/Web/Administrador/CadastroQuartoStore.java
@@ -5,10 +5,26 @@
  */
 package br.com.Sistema.Web.Administrador;
 
+import br.com.Sistema.Bean.QuartoBean;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author Alunos
  */
-public class CadastroQuartoStore {
+public class CadastroQuartoStore extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        QuartoBean quarto =new QuartoBean();
+    }
+    
+    
+    
     
 }
