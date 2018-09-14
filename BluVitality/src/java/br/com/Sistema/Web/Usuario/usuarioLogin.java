d2bd7253ed257d7288a900ea6b99077ddf1dc15f
RafhaHarada <RafhaHarada@gmail.com>
2018-09-14 01:09:46 -0300

    Login de usuario separado

---
+++ BluVitality/src/java/br/com/Sistema/Web/Usuario/usuarioLogin.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.Sistema.Web.Usuario;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet("/usuario/login")
+public class usuarioLogin extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        resp.setContentType("text/html;charset=UTF-8");
+        req.getRequestDispatcher("/usuario/login.jsp").include(req, resp);
+    }
+}
