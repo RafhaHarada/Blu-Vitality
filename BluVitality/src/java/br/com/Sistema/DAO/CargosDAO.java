5c12409fe304f31ec88694588a16bd9551edc61d
cidmardsr <38558577+cidmardsr@users.noreply.github.com>
2018-09-14 11:28:43 -0300

    Continuando

--- BluVitality/src/java/br/com/Sistema/DAO/CargosDAO.java
+++ BluVitality/src/java/br/com/Sistema/DAO/CargosDAO.java
@@ -14,96 +14,103 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
  * @author Cidmar
  */
 public class CargosDAO {
+
     public List<CargosBean> obterTodos() {
         List<CargosBean> cargos = new ArrayList<>();
         String sql = "SELECT id, nome, salario, carga_horaria FROM cargos";
-        
-        try{
+
+        try {
             Statement st = Conexao.abrirConexao().createStatement();
             st.execute(sql);
             ResultSet resultSet = st.getResultSet();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 CargosBean cargo = new CargosBean();
                 cargo.setId(resultSet.getInt("id"));
                 cargo.setNome(resultSet.getString("nome"));
                 cargo.setSalario(resultSet.getDouble("salario"));
                 cargo.setCarga_horaria(resultSet.getDate("carga_horaria"));
             }
-            }catch(SQLException e){
-                    e.printStackTrace();
-            }finally{
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
             Conexao.fecharConexao();
-        }return cargos;
+        }
+        return cargos;
     }
-    
-    public CargosBean obterPeloId(int id){
+
+    public CargosBean obterPeloId(int id) {
         CargosBean cargo = null;
         String sql = "SELECT id, nome, salario, carga_horaria FROM cargos WHERE id = ?";
-        try{
+        try {
             PreparedStatement ps = Conexao.abrirConexao().prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet resultSet = ps.getResultSet();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 cargo = new CargosBean();
                 cargo.setId(resultSet.getInt("id"));
                 cargo.setNome(resultSet.getString("nome"));
                 cargo.setSalario(resultSet.getDouble("salario"));
                 cargo.setCarga_horaria(resultSet.getDate("carga_horaria"));
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             Conexao.fecharConexao();
-        }return cargo;
+        }
+        return cargo;
     }
-    
+
     public int adicionar(CargosBean cargo) {
         String sql = "INSERT INTO cargos (id, nome, salario, carga_horaria) "
                 + "VALUES(?,?,?,?)";
-        try{
+        try {
             PreparedStatement ps = Conexao.abrirConexao().prepareStatement(sql, RETURN_GENERATED_KEYS);
             ps.setString(1, cargo.getNome());
             ps.setDouble(2, cargo.getSalario());
             ps.setDate(3, cargo.getCarga_horaria());
-            
+
             ps.execute();
             ResultSet resultSet = ps.getGeneratedKeys();
-            if(resultSet.last()){
+            if (resultSet.last()) {
                 return resultSet.getInt(1);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             Conexao.fecharConexao();
-        }return -1;
+        }
+        return -1;
     }
-    public boolean alterar(CargosBean cargo){
-        try{
+
+    public boolean alterar(CargosBean cargo) {
+        try {
             String sql = "UPDATE cargos SET nome = ?, salario = ?, carga_horaria = ? WHERE id = ?";
             PreparedStatement ps = Conexao.abrirConexao().prepareStatement(sql);
             ps.setString(1, cargo.getNome());
             ps.setDouble(2, cargo.getSalario());
             ps.setDate(3, cargo.getCarga_horaria());
             ps.setInt(4, cargo.getId());
-            
-            return ps.executeUpdate() ==1;
-        }catch(SQLException e){
+
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             Conexao.fecharConexao();
-        }return false;
+        }
+        return false;
     }
-    
-    public boolean apagar(int id){
+
+    public boolean apagar(int id) {
         String sql = "DELETE FROM cargos WHERE id = ?";
-        try{
+        try {
             PreparedStatement ps = Conexao.abrirConexao().prepareStatement(sql);
             ps.setInt(1, id);
-            return ps.executeUpdate() ==1;
-        }catch(SQLException e){
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             Conexao.fecharConexao();
-        }return false;
+        }
+        return false;
     }
 }
