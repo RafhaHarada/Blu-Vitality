54c48223cdd219ab1b614b8ad7a24e18c23e01f9
RafhaHarada <RafhaHarada@gmail.com>
2018-09-14 01:19:56 -0300

    Signout redirect

--- BluVitality/web/resources/js/custom.js
+++ BluVitality/web/resources/js/custom.js
@@ -135,6 +135,7 @@ function onSignIn(googleUser) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+        window.location = "index.jsp";
     });
 }
 
