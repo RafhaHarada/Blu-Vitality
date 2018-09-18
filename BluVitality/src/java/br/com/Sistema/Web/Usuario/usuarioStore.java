178b6ca64e331f7d051efaaa631c2b00d7581ff1
RafhaHarada <RafhaHarada@gmail.com>
2018-09-17 23:07:03 -0300

    Otimização de arquivo

--- BluVitality/src/java/br/com/Sistema/Web/Usuario/usuarioStore.java
+++ BluVitality/src/java/br/com/Sistema/Web/Usuario/usuarioStore.java
@@ -22,36 +22,23 @@ public class usuarioStore extends HttpServlet {
         UsuariosBean usuario = new UsuariosBean();
 
         usuario.setNome(req.getParameter("nome-completo"));
-
         usuario.setSexo(req.getParameter("sexo").charAt(0));
-
         usuario.setCpf(req.getParameter("cpf"));
-
         usuario.setRg(req.getParameter("rg"));
-
         usuario.setEstado_civil(req.getParameter("estado-civil"));
-
         usuario.setTelefone(req.getParameter("telefone"));
-
         usuario.setEmail(req.getParameter("email"));
-
         usuario.setEndereco(req.getParameter("endereco"));
-
         usuario.setComplemento(req.getParameter("complemento"));
         usuario.setContato_emergencia(req.getParameter("contato-emergencia"));
-
         usuario.setUf(req.getParameter("uf"));
-
         usuario.setNaturalidade(req.getParameter("naturalidade"));
-
         String dataBr[] = req.getParameter("data-nascimento").split("/");
         String dataEn = dataBr[2] + "-" + dataBr[1] + "-" + dataBr[0];
         usuario.setData_nascimento(java.sql.Date.valueOf(dataEn));
         int idade = Period.between(LocalDate.parse(dataEn), LocalDate.now()).getYears();
         usuario.setIdade((byte) idade);
-
         usuario.setTipo_sanguineo(req.getParameter("tipo-sanguineo"));
-
         usuario.setConvenio(req.getParameter("convenio"));
 
         usuario.setId(new UsuariosDAO().adicionar(usuario));
